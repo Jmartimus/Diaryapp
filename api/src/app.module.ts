77985f15f08cbf75f53c7171106470b28edd2d6d
Jonathan Martinez <Jmartinez070589@gmail.com>
2021-05-25 07:08:18 -0500

    auth module creation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PostsModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [PostsModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
 })
 export class AppModule {}
